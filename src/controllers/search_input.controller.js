dff1bb5765219b1e934e0e350aed4a285457ae28
patelrohan224 <rohan_fw11_289@masai.school>
2021-09-30 14:55:58 +0530

    aa

--- src/controllers/search_input.controller.js
+++ src/controllers/search_input.controller.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const SearchInput= require("../models/search_input_model");
+const Search = require("../models/search.model");
+
+router.get("", async (req, res) => {
+  const  items = await Search.find().lean().exec();
+  const  items2 = await Search.find({city:"Chennai"}).lean().exec();
+     res.render("search.ejs", {
+        items,
+        items2
+    })
+    // return res.send({ items });
+}) 
+
+
 
 router.post("", async (req, res) => {
     const items = await SearchInput.create(req.body);
     console.log("sdeew");
     console.log(items);
-    return res.send({items})
+ res.redirect("/cars")
   });
 
 module.exports = router;
