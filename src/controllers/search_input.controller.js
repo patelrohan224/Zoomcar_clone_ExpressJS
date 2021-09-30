a7ff951d523360c8b9ca51ef3ebb8487d3abec75
patelrohan224 <rohan_fw11_289@masai.school>
2021-09-30 11:03:16 +0530

    a

--- src/controllers/search_input.controller.js
+++ src/controllers/search_input.controller.js
@@ -5,9 +5,11 @@ const router = express.Router();
 
 const SearchInput= require("../models/search_input_model");
 
-router.post("/", async (req, res) => {
+router.post("", async (req, res) => {
     const items = await SearchInput.create(req.body);
-    
+    console.log("sdeew");
+    console.log(items);
+    return res.send({items})
   });
 
 module.exports = router;
