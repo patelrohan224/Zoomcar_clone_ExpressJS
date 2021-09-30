6be25ab2f4ccac6fa174257d7df6a9e1d9130544
Priya Gupta <88648418+Priya31g@users.noreply.github.com>
2021-09-30 14:36:11 +0530

    committing changes

--- src/controllers/search_input.controller.js
+++ src/controllers/search_input.controller.js
@@ -1,15 +1,14 @@
-
 const express = require("express");
 
 const router = express.Router();
 
-const SearchInput= require("../models/search_input_model");
+const SearchInput = require("../models/search_input_model");
 
-router.post("", async (req, res) => {
+router.post("", async(req, res) => {
     const items = await SearchInput.create(req.body);
     console.log("sdeew");
     console.log(items);
-    return res.send({items})
-  });
+    return res.send({ items })
+});
 
 module.exports = router;
