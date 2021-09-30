ba9c359e17491f1c3c57b0b59be493c5a061ea26
shashank0194 <87421851+shashank0194@users.noreply.github.com>
2021-09-30 11:38:01 +0530

    update

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -3,13 +3,19 @@ const router = express.Router();
 
 const Signup = require("../models/signup.model");
 
-router.post("", async(req, res) => {
-    const signup = await Signup.create(req.body);
-    id = signup._id
-        //  return res.status(201).send({
-        //signup: signup._id
-        //})
-})
+// router.post("", async(req, res) => {
+//     const signup = await Signup.create(req.body);
+
+//     // id = signup._id
+//         //  return res.status(201).send({
+//         //signup: signup._id
+//         //})
+
+//     res.render("home.ejs",{
+//         signup
+//     })
+
+// })
 
 
 router.get("", async(req, res) => {
@@ -17,18 +23,18 @@ router.get("", async(req, res) => {
     return res.render("home.ejs")
 })
 
-// router.get("/:id", async (req, res) => {
-//     const signup = await Signup.findById(req.params.id).lean().exec();
+// // router.get("/:id", async (req, res) => {
+// //     const signup = await Signup.findById(req.params.id).lean().exec();
+// //     return res.status(200).send({ signup })
+// // })
+
+// router.patch("/:id", async(req, res) => {
+
+//     // const signup = await Signup.findByIdAndUpdate(req.params.id, req.body, ({ new: true })).lean().exec();
+//     const signup = await Signup.findByIdAndUpdate(('http://localhost:3535/signup/' + req.params.id), req.body, { new: true });
+
 //     return res.status(200).send({ signup })
 // })
-
-router.patch("/:id", async(req, res) => {
-
-    // const signup = await Signup.findByIdAndUpdate(req.params.id, req.body, ({ new: true })).lean().exec();
-    const signup = await Signup.findByIdAndUpdate(('http://localhost:3535/signup/' + req.params.id), req.body, { new: true });
-
-    return res.status(200).send({ signup })
-})
 
 
 module.exports = router
