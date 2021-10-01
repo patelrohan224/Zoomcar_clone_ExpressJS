1eb9705bf84286039195199b60087f78f338cdb5
shashank0194 <87421851+shashank0194@users.noreply.github.com>
2021-10-01 18:47:17 +0530

    car_page

--- public/js/car.js
+++ public/js/car.js
@@ -1,24 +1,7 @@
-let db_arr=localStorage.getItem("pick_drop_points")
-        var new_db_location=JSON.parse(db_arr)
-        console.log(new_db_location);
-        console.log(new_db_location[0].d_city);
-        document.querySelector(".pick_city").innerText=new_db_location[0].p_city
-        document.querySelector(".drop_city").innerText=new_db_location[0].d_city
-        document.querySelector(".d_point_car").innerText=new_db_location[0].d_location
-        document.querySelector(".p_point_car").innerText=new_db_location[0].p_location
-      
-        document.querySelector(".left_date").innerText=new_db_location[0].p_date
-        document.querySelector(".right_date").innerText=new_db_location[0].d_date
-        document.querySelector(".p_day").innerText=new_db_location[0].p_month
-        document.querySelector(".d_day").innerText=new_db_location[0].d_month
-        document.querySelector(".s_time_date").innerText=new_db_location[0].p_time
-        document.querySelector(".e_time_date").innerText=new_db_location[0].d_time
 
-   
-    function signup_roh() {
-        
-        document.querySelector('.cont').style.display = "flex"
-        
-    }
-   
-   
+function signup_roh() {
+
+    document.querySelector('.cont').style.display = "flex"
+
+}
+
